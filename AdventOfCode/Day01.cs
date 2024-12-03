a8568b15a29ed40ecc9ea19f8b5b8651a399610a
Myles Johnson <myles.s.johnson@gmail.com>
2024-12-03 17:47:45 -0500

    Day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,62 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{Solve_1_First_Approach()}");
+    public override ValueTask<string> Solve_2() => new($"{Solve_2_First_Approach()}");
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    private int Solve_1_First_Approach()
+    {
+        List<int> left = new();
+        List<int> right = new();
+
+        foreach (var line in _input)
+        {
+            var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries ^ StringSplitOptions.TrimEntries);
+
+            left.Add(int.Parse(split[0]));
+            right.Add(int.Parse(split[1]));
+        }
+
+        left.Sort();
+        right.Sort();
+
+        int distance = 0;
+        for (int i = 0; i < left.Count; i++)
+        {
+            distance += Math.Abs(left[i] - right[i]);
+        }
+
+        return distance;
+    }
+
+    private int Solve_2_First_Approach()
+    {
+        List<int> left = new();
+        Dictionary<int, int> right = new(); // number, count
+
+        foreach (var line in _input)
+        {
+            var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries ^ StringSplitOptions.TrimEntries);
+
+            left.Add(int.Parse(split[0]));
+            
+            int rightValue = int.Parse(split[1]);
+            right[rightValue] = right.GetValueOrDefault(rightValue, 0) + 1;
+        }
+
+        int similarity = 0;
+        foreach(var number in left)
+        {
+            similarity += number * right.GetValueOrDefault(number, 0);
+        }
+
+        return similarity;
+    }
 }
