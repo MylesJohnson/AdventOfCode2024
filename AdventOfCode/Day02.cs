887ba998c5a827ec6c2d76b07f8add0d548da0c9
Myles Johnson <myles.s.johnson@gmail.com>
2024-12-04 00:01:43 -0500

    Day2 Part 1

---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day02()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Solve_1_First_Approach()}");
+    public override ValueTask<string> Solve_2() => new($"{Solve_2_First_Approach()}");
+
+    private int Solve_1_First_Approach()
+    {
+        int safe = 0;
+
+        foreach(string report in _input)
+        {
+            var levels = report.Split(' ').Select(int.Parse).ToArray();
+
+            bool? increasing = null;
+            for (int i = 0; i < levels.Length - 1; i++)
+            {
+                if (increasing == null)
+                {
+                    increasing = levels[i] < levels[i + 1];
+                }
+                else if (increasing.Value && levels[i] > levels[i + 1])
+                {
+                    safe -= 1; // Since we always increment safe, we need to decrease it when its unsafe to cancel it out
+                    break;
+                }
+                else if (!increasing.Value && levels[i] < levels[i + 1])
+                {
+                    safe -= 1; // Since we always increment safe, we need to decrease it when its unsafe to cancel it out
+                    break;
+                }
+
+                if (Math.Abs(levels[i] - levels[i + 1]) > 3 || Math.Abs(levels[i] - levels[i + 1]) < 1)
+                {
+                    safe -= 1; // Since we always increment safe, we need to decrease it when its unsafe to cancel it out
+                    break;
+                }
+            }
+
+            safe += 1;
+        }
+
+        return safe;
+    }
+
+    private int Solve_2_First_Approach()
+    {
+        throw new SolvingException("Not implemented");
+    }
+}
